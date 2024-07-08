d975dbf763e3ed48cd969a2eb22f2d9197f0b07e
dalfiannur <dalfiannur@gmail.com>
2024-07-08 22:32:54 +0700

    Homepage

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -5,6 +5,9 @@
     <livewire:home-page.explore-information />
     <livewire:home-page.banner-slider />
     <livewire:home-page.video-series />
+    <livewire:home-page.video-slider />
+    <livewire:home-page.recent-news />
+    <livewire:partials.footer />
 </div>
 
 @pushOnce('styles')
@@ -13,4 +16,5 @@
 
 @pushOnce('script-loaders')
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide-extension-grid@0.4.1/dist/js/splide-extension-grid.min.js"></script>
 @endPushOnce
