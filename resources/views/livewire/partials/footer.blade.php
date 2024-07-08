4b9f473f2f2c8ea316a6a44e50aa791717592f96
dalfiannur <dalfiannur@gmail.com>
2024-07-08 22:32:43 +0700

    Footer

---
+++ resources/views/livewire/partials/footer.blade.php
@@ -0,0 +1,76 @@
+<div class="pt-10 flex flex-col gap-10 border-t">
+    <div class="px-[120px] flex justify-between">
+        <div class="flex flex-col gap-4 max-w-[387px]">
+            <livewire:partials.logo />
+            <div class="flex flex-col gap-2">
+                <p class="font-semibold">Sekretaris Kota Jakarta Timur</p>
+                <p class="text-sm font-medium text-gray-500">Pejabat Pengelola Informasi Dan Dokumentasi Kota
+                    Administrasi Jakarta Timur</p>
+                <p class="text-sm font-medium text-gray-500">Kantor Walikota Jakarta Timur, Gedung A Lantai 1</p>
+            </div>
+        </div>
+        <div class="flex flex-col gap-6">
+            <h6 class="font-semibold text-gray-500">Statistik Kunjungan</h6>
+            <div class="flex flex-col gap-6 font-normal text-gray-500">
+                <p>
+                    10 Hari Ini
+                </p>
+                <p>
+                    1000
+                </p>
+            </div>
+        </div>
+        <div class="flex flex-col gap-6">
+            <h6 class="font-semibold text-gray-500">Hubungi Kami</h6>
+            <ul class="flex flex-col gap-6 font-normal text-gray-500">
+                <li class="flex gap-2">
+                    <div>i</div>
+                    <p>( 021 ) 4800487</p>
+                </li>
+                <li class="flex gap-2">
+                    <div>i</div>
+                    <p>suratwalikotajkt@jakarta.go.id</p>
+                </li>
+            </ul>
+        </div>
+        <div class="flex flex-col gap-6">
+            <h6 class="font-semibold text-gray-500">Sosial Media</h6>
+            <div class="flex flex-col gap-6 font-normal text-gray-500">
+                <div class="flex flex-col gap-2">
+                    <h6 class="font-normal">PPID Jakarta Timur</h6>
+                    <ul class="flex gap-6">
+                        <li>
+                            <a href="#">FB</a>
+                        </li>
+                        <li>
+                            <a href="#">X</a>
+                        </li>
+                        <li>
+                            <a href="#">YT</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <h6 class="font-normal">Kota Jakarta Timur</h6>
+                    <ul class="flex gap-6">
+                        <li>
+                            <a href="#">FB</a>
+                        </li>
+                        <li>
+                            <a href="#">X</a>
+                        </li>
+                        <li>
+                            <a href="#">YT</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="bg-gray-300 w-[300px] h-[192px]"></div>
+        </div>
+    </div>
+    <div class="flex justify-center bg-gradient-to-b from-[#FDEAF6] to-white p-3">
+        <p class="text-[#EB30A2] font-medium">Copyright <span class="font-extrabold">Jakarta Timur</span> Sudin Kominfotik JT</p>
+    </div>
+</div>
